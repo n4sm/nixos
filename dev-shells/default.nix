d1beffc4b5f7536f16aee57d6322788d4a976d28
n4sm <n4sm@pm.me>
2024-06-22 23:07:47 +0200

    hexo

--- dev-shells/default.nix
+++ dev-shells/default.nix
@@ -1,6 +1,7 @@
 { pkgs, packages, }:
 {
   clang = pkgs.callPackage ./clang.nix { };
+  hexo = pkgs.callPackage ./hexo.nix { };
   pwn = pkgs.callPackage ./pwn.nix { pkgs = pkgs; packages = packages; };
   python = pkgs.callPackage ./python.nix { };
 }
